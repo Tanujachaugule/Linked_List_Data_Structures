f6f31ed88b8a62bba56903dd002d8265ada9aaa7
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-02-20 08:07:52 +0530

    Added: Ability to Adding Node in Linked List

--- src/com/linkedlist/AddingNode.java
+++ src/com/linkedlist/AddingNode.java
@@ -1,2 +1,27 @@
-package com.linkedlist;public class AddingNode {
+package com.linkedlist;
+
+public class AddingNode {
+    LinkedList.Node head;
+    static class Node {
+        int value;
+        LinkedList.Node next;
+        Node(int d) {
+            value = d;
+            next = null;
+        }
+    }
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.head = new LinkedList.Node(70);
+        LinkedList.Node second = new LinkedList.Node(30);
+        LinkedList.Node third = new LinkedList.Node(56);
+        linkedList.head.next = second;
+        second.next = third;
+        System.out.print("LinkedList: ");
+        while (linkedList.head != null) {
+            System.out.print(linkedList.head.value + " ");
+            linkedList.head = linkedList.head.next;
+        }
+    }
 }
+
